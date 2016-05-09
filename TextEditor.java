a98c0c1c21dd149ffe9d447db6fdf6783e27720f
notoaki15 <siderop0ne0@gmail.com>
2016-05-09 15:45:26 +0900

    Create TextEditor.java

---
+++ TextEditor.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class TE extends JFrame{
+	
+	public static void main(String[]args){
+		
+		JTextArea area = new JTextArea();
+		area.setBackground(Color.black);
+		area.setForeground(Color.green);
+		
+		
+		JFrame frame = new JFrame();
+		frame.setSize(400,400);
+		frame.setBackground(Color.BLACK);
+		frame.setContentPane(area);
+		frame.setVisible(true);
+		
+	}
+
+}
